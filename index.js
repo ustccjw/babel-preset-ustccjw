b2110dfd284fb8ed01f2d8b2b5cc7441dd4965df
ustccjw <317713370@qq.com>
2017-07-11 15:32:21 +0800

    remove babel-runtime

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ import presetStage0 from 'babel-preset-stage-0'
 import presetReact from 'babel-preset-react'
 import pluginAddModuleExports from 'babel-plugin-add-module-exports'
 import pluginTransformDecoratorsLegacy from 'babel-plugin-transform-decorators-legacy'
-import pluginTransformRuntime from 'babel-plugin-transform-runtime'
 
 export default {
   presets: [presetEnv, presetStage0, presetReact],
-  plugins: [pluginAddModuleExports, pluginTransformDecoratorsLegacy, [pluginTransformRuntime, {
-    polyfill: false,
-  }]],
+  plugins: [pluginAddModuleExports, pluginTransformDecoratorsLegacy],
 }
